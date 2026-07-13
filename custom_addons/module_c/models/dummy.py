e66b717215d73ea243db56bdcf09bebf12d9bb91
  Yurii <yurii@MacBook-Pro-Yurii.local>
2026-07-13 13:15:43 +0300

    test: add new module_c not present on develop

---
+++ custom_addons/module_c/models/dummy.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class DummyC(models.Model):
+    _name = 'module.c.dummy'
+    _description = 'Dummy Model C'
+
+    name = fields.Char()
+    active = fields.Boolean(default=True)
